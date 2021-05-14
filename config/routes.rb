c8004c57af44dfb8abb44ec437b6f6c7019a49f7
seedsat <e.satoshi.bi@gmail.com>
2021-05-14 21:53:07 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
+  ## ルーティングをネストすることでどこのルームでメッセージを送るかをURLで判断する。
+  ## アソシエーションの関係にあるもの同志を紐づける
+  ## とても大切な知識である
+  resources :rooms, only: [:new, :create] do
+    ## メッセージを送るにはどのルームでメッセージを送るのかを判別しなければならない
+    ## 親にroomsコントローラーにしてネストすることでルームのidをURLから取得することができる
+    resources :messages, only: [:index, :create]
+  end
 end
