4662925b60dd0435d3c339bc854106e504f5edfa
seedsat <e.satoshi.bi@gmail.com>
2021-05-14 22:31:47 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   ## ルーティングをネストすることでどこのルームでメッセージを送るかをURLで判断する。
   ## アソシエーションの関係にあるもの同志を紐づける
   ## とても大切な知識である
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     ## メッセージを送るにはどのルームでメッセージを送るのかを判別しなければならない
     ## 親にroomsコントローラーにしてネストすることでルームのidをURLから取得することができる
     resources :messages, only: [:index, :create]
