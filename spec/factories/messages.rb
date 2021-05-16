b215c69b7be39eb16fdd26f36b77692ab865f51c
seedsat <e.satoshi.bi@gmail.com>
2021-05-16 16:28:03 +0900

    各モデルのテスト

---
+++ spec/factories/messages.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :message do
+    content {Faker::Lorem.sentence}
+    association :user
+    association :room
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/004_FRIENDS.png'), filename: '004_FRIENDS.png' )
+    end
+  end
+end
